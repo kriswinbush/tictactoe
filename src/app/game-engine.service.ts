ff00d868425d2a8285815464d5f484997c50dc8a
Kris Winbush <khz@kriss-mbp.lan>
2018-06-25 08:47:44 -0700

    removed console logs from gameEngine

--- src/app/game-engine.service.ts
+++ src/app/game-engine.service.ts
@@ -4,6 +4,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class GameEngineService {
+  constructor() {}
   numOfPlayers = [
     {value: 1, viewValue: 'One'},
     {value: 2, viewValue: 'Two'},
@@ -35,7 +36,7 @@ export class GameEngineService {
   };
 
   playerMarkers = ['color1', 'color2', 'color3'];
-  numRows = 4;
+  numRows = 5;
   numColumns = 4;
   numPlayers = 1;
   players = [];
@@ -75,6 +76,7 @@ export class GameEngineService {
     }
     this.board.push(row)
   }
+
   generateBoard() {
     for( let i= 0; i < this.numRows; i++) {
       this.generateRow();
@@ -91,6 +93,7 @@ export class GameEngineService {
   checkPlayerWon() {
     return this.checkMatrixForMatchs(this.currentPlayer);
   }
+
   checkMatrixForMatchs(player) {
     var playerWonTheGame = false;
     var playerHasColumnWin = [];
@@ -98,7 +101,8 @@ export class GameEngineService {
     let playerMatrix = this.board.map(boardRowArray => {
       return boardRowArray.map(e => e.owner.indexOf(player.name))
     })
-    //all values in row the same
+
+    //all values in row are the same
     for(let i =0; i < playerMatrix.length; i++) {
       //first test if all values in this row are the same
       var rowCheck = playerMatrix[i].every(elVal => elVal === playerMatrix[i][0] && elVal !== -1);
@@ -106,8 +110,9 @@ export class GameEngineService {
        playerWonTheGame = true;
        break;
      }
-    }
-    //all values in a column the same
+    };
+
+    //all values in a column are the same
     playerMatrix.forEach((rowEl) => {
       if( rowEl.indexOf(0) !== -1) {
         playerHasColumnWin.push(rowEl.indexOf(0))
@@ -118,57 +123,27 @@ export class GameEngineService {
       }
     });
 
+    //Four in a square wins the game
     playerMatrix.forEach((rowEl, idx) => {
-      console.log(playerMatrix);
-      console.log('iteration index: ', idx)
       var i0 = rowEl.indexOf(0);
       if(i0 != -1) {
         var i1 = rowEl[i0 + 1]
         if(i1 != -1){
-          console.log('2 player squares in a row')
-          //check row length???
           var nextIdx = idx + 1;
           var prevIdx = idx - 1;
-          console.log("board length",this.board.length)
-          console.log("player matrix length",playerMatrix.length)
-          console.log("next Index", nextIdx)
-          console.log('orev Index', prevIdx)
           if(nextIdx >= playerMatrix.length || prevIdx == -1) {
-            console.log('no rows left')
+            console.log('no rows before of after this row')
           } else {
-            console.log("playerMatrix next row nextIdx first element 0 value: ",playerMatrix[nextIdx][i0]);
-            console.log("playerMatrix next row nextIdx next element 0 value: ",playerMatrix[nextIdx][i0 + 1]);
-            console.log("playerMatrix next row prevIdx first element 0 value: ",playerMatrix[prevIdx][i0]);
-            console.log("playerMatrix next row prevIdx next element 0 value: ", playerMatrix[prevIdx][i0 + 1]);
             if( (playerMatrix[nextIdx][i0] == 0 && playerMatrix[nextIdx][i0 + 1] == 0) || (playerMatrix[prevIdx][i0] == 0 && playerMatrix[prevIdx][i0 + 1] == 0)) {
               console.log("winner");
               playerWonTheGame = true;
             }
-            console.log(playerMatrix[nextIdx])
           }
         }
-        console.log(i0)
       }
     })
+    player.isWinner = true;
     return playerWonTheGame;
   }
-  comp(arr1, arr2) {
-    var sqrComp = false;
-    var idx = arr1[0];
-
-    if(arr1[0] === arr2[idx +1]) {
-      console.log('im going to compare')
-      let w = arr1.reduce((o, item, i)=> {
-        if(arr2.indexOf(item) ) {
-          console.log(arr2.indexOf(item))
-        }
-        console.log(item == arr2[i])
-        return;
-      })
-    } else {
-      console.log('passing on compare')
-    }
-    return sqrComp;
-  }
-  constructor() {}
+  
 }
